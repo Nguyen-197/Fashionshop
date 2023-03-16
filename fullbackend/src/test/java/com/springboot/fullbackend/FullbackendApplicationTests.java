ec81441ab3b9aee1ea1afc300962089ded2e87df
nguyen-197 <hellonguyen197@gmail.com>
2023-03-16 13:02:20 +0700

    first commit

---
+++ fullbackend/src/test/java/com/springboot/fullbackend/FullbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.fullbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
